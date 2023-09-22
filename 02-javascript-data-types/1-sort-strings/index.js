a299c88b815a045d0eb80f6d309d52107d1d6a6d
Slav <shutrow@rambler.ru>
2023-09-22 22:01:49 +0400

    HW2 mistakes corrected

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -6,32 +6,14 @@
  */
 export function sortStrings(arr, param = 'asc') {
   const newArr = [...arr];
-  const letterCodes =
-  "АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯяAaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz"
-  .split("");
+  const locales = ['ru-RU-u-kf-upper', 'en-US-u-kf-upper'];
 
-  if (param === "desc") {
-    letterCodes.reverse();
+  if (param === 'asc') {
+    newArr.sort((x, y) => x.localeCompare(y, locales));
+  } else if (param === 'desc') {
+    newArr.sort((x, y) => y.localeCompare(x, locales));
   }
-
-  newArr.sort((a, b) => {
-    for (let i = 0; i < Math.min(a.length, b.length); i++) {
-      if (a[i] !== b[i]) {
-        return letterCodes.indexOf(a[i]) - letterCodes.indexOf(b[i]);
-      }
-    }
-  });
 
   return newArr;
 }
 
-// sortStrings(['абрикос', 'Абрикос', 'яблоко', 'Яблоко', 'ёжик', 'Ёжик'], 'asc'); // ['Абрикос', 'абрикос', 'Ёжик', 'ёжик', 'Яблоко', 'яблоко']
-// sortStrings(['apple', 'Apple', 'banana', 'Banana', 'orange', 'Orange']);
-// sortStrings([
-//   "Детский велосипед Lexus Trike Racer Trike",
-//   "ТВ тюнер D-COLOR  DC1301HD",
-//   "Соска (пустышка) NUK 10729357",
-//   "Powerbank аккумулятор Hiper SP20000",
-//   "Соска (пустышка) Philips SCF182/12",
-
-// ]);
