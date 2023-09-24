2b5250a99d812f008d1e44406d6c3ac58572f00c
Slav <shutrow@rambler.ru>
2023-09-24 19:30:54 +0400

    HW3 - 1&2 tasks done

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,14 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
+  if (typeof obj === "undefined") {
+    return;
+  }
+  const inverted = [];
+  Object.entries(obj).forEach((entry) => {
+    inverted.push(([entry[0], entry[1]] = [entry[1], entry[0]]));
+  });
 
+  return Object.fromEntries(inverted);
 }
+
