4a49dfaf83a5c80b2451619037cb7d2ca156e65c
Slav <shutrow@rambler.ru>
2023-09-26 20:43:39 +0400

    HW3 tasks 3-4 done

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,21 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  let res = "";
+  let curSize;
 
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] !== string[i - 1]) {
+      curSize = 0;
+    }
+
+    if (curSize >= size) {
+      continue;
+    }
+
+    res += string[i];
+    curSize++;
+  }
+
+  return res;
 }
