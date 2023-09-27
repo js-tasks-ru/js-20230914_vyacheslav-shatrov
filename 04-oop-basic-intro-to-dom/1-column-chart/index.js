92c9c42509e2b22488ac0c951642e3e50dfc467b
Slav <shutrow@rambler.ru>
2023-09-27 23:34:12 +0400

    04 Chart class in progress

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,3 +1,64 @@
 export default class ColumnChart {
+  constructor(options) {
+    this.data = options.data || [];
+    this.label = options.label || '';
+    this.value = options.value || 0;
+    this.link = options.link || '';
 
+    this.element = this.createElement();
+  }
+
+  prepareCols() {
+    this.colData = this.data.map(value => ({
+      value: value / 2,
+      tooltipValue: value
+    }));
+
+    return this.colData.map(col => {
+      return `<div style="--value: ${col.value}" data-tooltip="${col.tooltipValue}%"></div>`;
+    }).join('');
+  }
+
+  template() {
+    const loading = !this.data.length ? 'column-chart_loading' : '';
+    const link = this.link
+      ? `<a href="/${this.link}" class="column-chart__link">View all</a>`
+      : '';
+    const cols = this.prepareCols();
+
+    return (
+      `
+      <div class="column-chart ${loading}" style="--chart-height: 50">
+      <div class="column-chart__title">
+        Total ${this.label}
+        ${link}
+      </div>
+      <div class="column-chart__container">
+        <div data-element="header" class="column-chart__header">$${this.value}</div>
+        <div data-element="body" class="column-chart__chart">
+          ${cols}
+        </div>
+      </div>
+    </div>`
+    );
+  }
+
+  update(data) {
+    this.data = data;
+    this.createElement();
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+  }
+  createElement() {
+    const element = document.createElement('div');
+    element.innerHTML = this.template();
+
+    return element;
+  }
 }
