dcaf92ee25c629a69e3a21bd10594e83c1b766c2
Slav <shutrow@rambler.ru>
2023-09-29 22:42:16 +0400

    4HW problems with loader class

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,13 +1,17 @@
-const defaultParams = {
+const defaultOptions = {
   data: [],
+  label: 'New Label',
   link: '',
   value: 0,
 };
 
 export default class ColumnChart {
-  constructor(options = defaultParams) {
-    this.data = options.data;
-    this.label = options.label;
+
+  chartHeight = 50;
+
+  constructor(options = defaultOptions) {
+    this.data = options.data || defaultOptions.data;
+    this.label = options.label || defaultOptions.label ;
     this.value = options.value;
     this.link = options.link;
 
@@ -15,18 +19,23 @@ export default class ColumnChart {
   }
 
   prepareCols() {
+    const yMax = this.data.length ? Math.max(...this.data) : 0;
+    const yRatio = yMax ? this.chartHeight / yMax : 0;
+
     this.colData = this.data.map(value => ({
-      value: value / 2,
-      tooltipValue: value
+      colHeight: String(Math.floor(value * yRatio)),
+      tooltipValue: (value / yMax * 100).toFixed(0) + '%'
     }));
 
     return this.colData.map(col => {
-      return `<div style="--value: ${col.value}" data-tooltip="${col.tooltipValue}%"></div>`;
+      return `<div style="--value: ${col.colHeight}" data-tooltip="${col.tooltipValue}"></div>`;
     }).join('');
   }
 
   template() {
+
     const loading = !this.data.length ? 'column-chart_loading' : '';
+    console.log(loading)
     const link = this.link
       ? `<a href="/${this.link}" class="column-chart__link">View all</a>`
       : '';
@@ -34,7 +43,7 @@ export default class ColumnChart {
 
     return (
       `
-      <div class="column-chart ${loading}" style="--chart-height: 50">
+      <div class="column-chart ${loading}" style="--chart-height: ${this.chartHeight}">
       <div class="column-chart__title">Total ${this.label}${link}</div>
       <div class="column-chart__container">
         <div data-element="header" class="column-chart__header">$${this.value}</div>
@@ -51,7 +60,9 @@ export default class ColumnChart {
     this.createElement();
   }
 
-  remove() {
+  remove(
+
+  ) {
     this.element.remove();
   }
 
