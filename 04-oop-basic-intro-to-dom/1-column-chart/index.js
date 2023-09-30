ddb83edef83f9a4e5075e96ff9368af9d8307877
Slav <shutrow@rambler.ru>
2023-09-30 20:40:38 +0400

    HW4 done

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -14,8 +14,9 @@ export default class ColumnChart {
     this.label = options.label || defaultOptions.label ;
     this.value = options.value;
     this.link = options.link;
+    this.formatHeading = options.formatHeading;
 
-    this.element = this.createElement();
+    this.element = this.render();
   }
 
   prepareCols() {
@@ -32,26 +33,29 @@ export default class ColumnChart {
     }).join('');
   }
 
-  template() {
-
-    const loading = !this.data.length ? 'column-chart_loading' : '';
-    console.log(loading)
+  createTemplate() {
     const link = this.link
       ? `<a href="/${this.link}" class="column-chart__link">View all</a>`
       : '';
+    this.value = this.formatHeading ? this.formatHeading(this.value) : this.value;
     const cols = this.prepareCols();
 
     return (
       `
-      <div class="column-chart ${loading}" style="--chart-height: ${this.chartHeight}">
-      <div class="column-chart__title">Total ${this.label}${link}</div>
-      <div class="column-chart__container">
-        <div data-element="header" class="column-chart__header">$${this.value}</div>
-        <div data-element="body" class="column-chart__chart">
-          ${cols}
+      <div class="column-chart" style="--chart-height: ${this.chartHeight}">
+        <div class="column-chart__title">Total ${this.label}${link}</div>
+        <div class="column-chart__container">
+          <div
+            data-element="header"
+            class="column-chart__header"
+          >
+            ${this.value}
+          </div>
+          <div data-element="body" class="column-chart__chart">
+            ${cols}
+          </div>
         </div>
-      </div>
-    </div>`
+      </div>`
     );
   }
 
@@ -60,9 +64,7 @@ export default class ColumnChart {
     this.createElement();
   }
 
-  remove(
-
-  ) {
+  remove() {
     this.element.remove();
   }
 
@@ -72,8 +74,15 @@ export default class ColumnChart {
 
   createElement() {
     const element = document.createElement('div');
-    element.innerHTML = this.template();
+    if (!this.data.length) {
+      element.classList.add('column-chart_loading');
+    }
+    element.innerHTML = this.createTemplate();
 
     return element;
+  }
+
+  render() {
+    return this.createElement();
   }
 }
