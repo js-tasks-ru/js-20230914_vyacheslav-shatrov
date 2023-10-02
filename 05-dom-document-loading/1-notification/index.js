d5e64e88d611b77eff2efa49dfbf21c160a9a816
Slav <shutrow@rambler.ru>
2023-10-02 23:46:24 +0400

    5HW - more questions than answers

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,50 @@
 export default class NotificationMessage {
 
+  element;
+
+  constructor(title, options = {}) {
+    this.title = title || '';
+    this.duration = options.duration || 0;
+    this.type = options.type || 'error';
+
+    this.element = this.render();
+  }
+
+  createTemplate() {
+    return `
+      <div class="notification success" style="--value:${this.duration}s">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.type}</div>
+          <div class="notification-body">
+            ${this.title}
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  createElement() {
+    const element = document.createDocumentFragment();
+    element.innerHTML = this.createTemplate();
+    return element;
+  }
+
+  show() {
+    this.element = this.createElement();
+
+    return document.getElementById('btn1').append(this.element);
+    // setTimeout(() => {
+    //   this.destroy(element);
+    // }, this.duration);
+
+  }
+
+  render() {
+    return this.createElement();
+  }
+
+  destroy(element) {
+    element.remove();
+  }
 }
