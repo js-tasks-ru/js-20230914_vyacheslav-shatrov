80f7a17c24e02fd82309e2640b64e591d2fd12d6
Slav <shutrow@rambler.ru>
2023-10-03 23:48:19 +0400

    HW5 task 1 done

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,50 +1,56 @@
 export default class NotificationMessage {
 
-  element;
+  static currentMessage = null;
 
-  constructor(title, options = {}) {
-    this.title = title || '';
-    this.duration = options.duration || 0;
-    this.type = options.type || 'error';
+  constructor(title = '', options = {}) {
+    this.title = title;
+    this.duration = options.duration;
+    this.type = options.type;
 
-    this.element = this.render();
+    this.element = this.createElement();
   }
 
   createTemplate() {
     return `
-      <div class="notification success" style="--value:${this.duration}s">
-        <div class="timer"></div>
-        <div class="inner-wrapper">
-          <div class="notification-header">${this.type}</div>
-          <div class="notification-body">
-            ${this.title}
-          </div>
+      <div class="timer"></div>
+      <div class="inner-wrapper">
+        <div class="notification-header">${this.type}</div>
+        <div class="notification-body">
+          ${this.title}
         </div>
       </div>
     `;
   }
 
   createElement() {
-    const element = document.createDocumentFragment();
+    const element = document.createElement('div');
+    element.classList.add("notification", this.type);
+    element.setAttribute("style", `--value: ${this.duration / 1000}s;`);
     element.innerHTML = this.createTemplate();
     return element;
   }
 
-  show() {
-    this.element = this.createElement();
+  show(parentElement = document.body) {
+    if (NotificationMessage.currentMessage) {
+      NotificationMessage.currentMessage.destroy();
+    }
 
-    return document.getElementById('btn1').append(this.element);
-    // setTimeout(() => {
-    //   this.destroy(element);
-    // }, this.duration);
+    NotificationMessage.currentMessage = this;
 
+    parentElement.append(this.element);
+    this.timer = setTimeout(() => {
+      this.destroy();
+    }, this.duration);
   }
 
-  render() {
-    return this.createElement();
+  remove() {
+    this.element.remove();
   }
 
-  destroy(element) {
-    element.remove();
+  destroy() {
+    clearTimeout(this.timer);
+    this.timer = null;
+    this.remove();
+    NotificationMessage.lastMessage = null;
   }
 }
